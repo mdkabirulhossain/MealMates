38bc86d5e25e9ae005cfb828ca154cc900051ba5
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-11-16 19:20:33 +0600

    Testimonial component is completed

--- MealMates_Client/src/Pages/Home/Testimonials/Testimonials.jsx
+++ MealMates_Client/src/Pages/Home/Testimonials/Testimonials.jsx
@@ -8,13 +8,19 @@ import 'swiper/css/navigation';
 // import required modules
 import { Navigation } from 'swiper/modules';
 
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
+
+import { FaQuoteLeft } from "react-icons/fa";
+
 const Testimonials = () => {
     const [reviews, setReviews] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('review.json')
-        .then(res => res.json())
-        .then(data =>setReviews(data))
+            .then(res => res.json())
+            .then(data => setReviews(data))
     }, [])
     return (
         <div>
@@ -27,16 +33,23 @@ const Testimonials = () => {
                 </SectionTitle>
             </div>
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                
+
                 {
-                    reviews.map(review =><SwiperSlide
+                    reviews.map(review => <SwiperSlide
                         key={review._id}
-                        >
-                        <div className='m-24'>
-                            <p className='text-center'>{review.details}</p>
-                            <h3 className='text-2xl text-center text-orange-400'>{review.name}</h3>
-                            </div>   
-                        </SwiperSlide>)
+                    >
+                        <div className='my-10 mx-14 flex flex-col items-center'>
+
+                            <Rating 
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            <FaQuoteLeft className='w-24 h-24 pt-8'/>
+                            <p className='py-8'>{review.details}</p>
+                            <h3 className='text-2xl text-orange-400'>{review.name}</h3>
+                        </div>
+                    </SwiperSlide>)
                 }
             </Swiper>
         </div>
