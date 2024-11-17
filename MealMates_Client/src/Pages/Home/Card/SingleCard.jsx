4b3f3a7ce1a85ff7b947f20c27f9e6d777fef634
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-11-17 23:04:35 +0600

    Updated card title and button

--- MealMates_Client/src/Pages/Home/Card/SingleCard.jsx
+++ MealMates_Client/src/Pages/Home/Card/SingleCard.jsx
@@ -9,11 +9,11 @@ const SingleCard = ({ item }) => {
                     src={image}
                     alt={name} />
             </figure>
-            <div className="card-body">
+            <div className="card-body items-center text-center">
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <div className="card-actions">
+                <button className="btn btn-outline text-[#BB8506] bg-[#E8E8E8] border-[#BB8506] border-b-2 border-0 uppercase">Add To Cart</button>
                 </div>
             </div>
         </div>
