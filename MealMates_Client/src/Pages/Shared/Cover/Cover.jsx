a4a1fbdbfc544842fe167616492c380eeff387cc
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-11-18 23:48:53 +0600

    Add componet in menu and completed banner part. this is dynamic banner

--- MealMates_Client/src/Pages/Shared/Cover/Cover.jsx
+++ MealMates_Client/src/Pages/Shared/Cover/Cover.jsx
@@ -8,7 +8,7 @@ const Cover = ({img,title, text, height}) => {
                 backgroundImage: `url(${img})`,
             }}>
             <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-neutral-content text-white text-center">
+            <div className="hero-content text-white text-center">
                 <div className="px-96 py-16 bg-[#151515] opacity-40">
                     <h1 className="mb-5 text-5xl font-bold">{title}</h1>
                     <p className="mb-5">
