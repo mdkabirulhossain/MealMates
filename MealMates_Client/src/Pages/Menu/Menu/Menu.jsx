b15676795ce4638d02e59cb3648ca002ef0fe527
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-11-18 23:47:47 +0600

    Create dynamic cover Component

--- MealMates_Client/src/Pages/Menu/Menu/Menu.jsx
+++ MealMates_Client/src/Pages/Menu/Menu/Menu.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Helmet} from 'react-helmet-async';
+import Cover from '../../Shared/Cover/Cover';
+import cover_back_img from '../../../assets/menu/banner3.jpg'
 
 const Menu = () => {
     return (
@@ -7,7 +9,12 @@ const Menu = () => {
            <Helmet>
                 <title>MealMates | Menu</title>
            </Helmet>
-           <p>Menu</p>
+           <Cover 
+           img={cover_back_img}
+           title={"OUR MENU"}
+           text={"Would you like to try a dish?"}
+           height={"600"}
+           ></Cover>
         </div>
     );
 };
