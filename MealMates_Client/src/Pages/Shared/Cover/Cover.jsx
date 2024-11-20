455a4daf008d138ac4e2e2de5147a4cc7b7faffb
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-11-20 22:51:53 +0600

    Cover component is updated

--- MealMates_Client/src/Pages/Shared/Cover/Cover.jsx
+++ MealMates_Client/src/Pages/Shared/Cover/Cover.jsx
@@ -15,8 +15,8 @@ const Cover = ({img,title, text, height}) => {
                 height: `${height}px`,
             }}>
             <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-white text-center">
-                <div className="px-96 py-16 bg-[#151515] opacity-40">
+            <div className="hero-content text-white text-center w-full">
+                <div className="w-4/5 px-20 py-16 bg-[#151515] opacity-40">
                     <h1 className="mb-5 text-5xl font-bold">{title}</h1>
                     <p className="mb-5">
                         {text}
