385f6f824348b84094c1f57f831f9771079b7380
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-11-21 00:00:56 +0600

    View Menu page is completed

--- MealMates_Client/src/Pages/Menu/Menu/Menu.jsx
+++ MealMates_Client/src/Pages/Menu/Menu/Menu.jsx
@@ -7,12 +7,16 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import Popular from '../Popular/Popular';
 import dessert_back_img from '../../../assets/menu/dessert-bg.jpeg'
 import pizza_back_img from '../../../assets/menu/pizza-bg.jpg'
+import salad_back_img from '../../../assets/menu/salad-bg.jpg'
+import soup_back_img from '../../../assets/menu/soup-bg.jpg'
 
 const Menu = () => {
     const [menu] = useMenu();
     const offer = menu.filter(item => item.category ==="offered")
     const desserts = menu.filter(item => item.category ==="dessert")
     const Pizza = menu.filter(item => item.category ==="pizza")
+    const Salad = menu.filter(item => item.category ==="salad")
+    const Soup = menu.filter(item => item.category ==="soup")
     return (
         <div>
            <Helmet>
@@ -36,7 +40,7 @@ const Menu = () => {
            {/* Dessert  */}
            <Cover 
            img={dessert_back_img}
-           title={"Desserts"}
+           title={"DESSERTS"}
            text={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
            height={"700"}
            ></Cover>
@@ -47,14 +51,38 @@ const Menu = () => {
            {/* Pizza  */}
            <Cover 
            img={pizza_back_img}
-           title={"Pizza"}
+           title={"PIZZA"}
            text={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
            height={"700"}
            ></Cover>
 
-           {/* dessert food list  */}
+           {/*Pizza food list  */}
 
            <Popular items={Pizza}></Popular>
+
+           {/* Salad  */}
+           <Cover 
+           img={salad_back_img}
+           title={"SALAD"}
+           text={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+           height={"700"}
+           ></Cover>
+
+           {/*Salad food list  */}
+
+           <Popular items={Salad}></Popular>
+
+           {/* soup */}
+           <Cover 
+           img={soup_back_img}
+           title={"SOUP"}
+           text={"Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}
+           height={"700"}
+           ></Cover>
+
+           {/*soup food list  */}
+
+           <Popular items={Soup}></Popular>
         </div>
     );
 };
