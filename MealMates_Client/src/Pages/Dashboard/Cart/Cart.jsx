05960b8cc80976780d9985125360260bcd3bd3d6
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-01 18:25:19 +0600

    Created Card data Dynamic

--- MealMates_Client/src/Pages/Dashboard/Cart/Cart.jsx
+++ MealMates_Client/src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
+import useCart from '../../../hooks/useCart';
 
 const Cart = () => {
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((accumulator, currentItem) => {
+        return accumulator + currentItem.price;
+    }, 0);
     return (
-        <div>
-            <h1>My Cart</h1>
+        <div className='px-10 py-10'>
+            <div className='flex justify-between'>
+                <h2 className='text-2xl'>Items: {cart.length}</h2>
+                <h2 className='text-2xl'>Total Price: ${totalPrice} </h2>
+                <button className='bg-orange-400 px-4 py-1 rounded-md'>Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle h-12 w-12">
+                                                <img
+                                                    src={item.image}
+                                                    alt={item.name}
+                                                />
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </td>
+                                <td>
+                                    <div>
+                                        <div className="font-bold">{item.name}</div>
+
+                                    </div>
+                                </td>
+                                <td>${item.price}</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>)
+                        }
+
+
+                    </tbody>
+
+                </table>
+            </div>
         </div>
     );
 };
