5eec87da267fb985ef7fe08df4e747f62cc9ceaf
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-03 18:55:46 +0600

    Updated signup with goole

--- MealMates_Client/src/Pages/SignUp/SignUp.jsx
+++ MealMates_Client/src/Pages/SignUp/SignUp.jsx
@@ -57,9 +57,18 @@ const SignUp = () => {
         }
         const hadlegoogleLogin= ()=>{
             googleSignIn()
-            .then(result =>{
-                console.log(result.user);
-            })
+        .then(result =>{
+            console.log(result.user);
+            const userInfo ={
+                            email: result?.user?.email,
+                            name: result?.user?.displayName,
+                        }
+                       axiosPublic.post('/users', userInfo)
+                       .then(res =>{
+                        console.log(res.data)
+                        navigate(from, {replace: true});
+                       })
+        })
         }
 
     // const handleLogin = (event) => {
