d30f3868749ce7aa1391da364b7a8bd4e2237582
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-05 18:40:18 +0600

    Make users is created and successuflly deleted users and admin

--- MealMates_Client/src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ MealMates_Client/src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -3,10 +3,11 @@ import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import { FaTrash, FaUsers } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
@@ -14,6 +15,54 @@ const AllUsers = () => {
             return res.data;
         },
     })
+
+    const handleMakeAdmin = user => {
+        axiosSecure.patch(`/users/admin/${user._id}`)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.modifiedCount > 0) {
+                    refetch();
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: `${user.name} is an admin now`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+
+    }
+
+    const handleDeleteUser = user => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/users/${user._id}`)
+                    .then(res => {
+                        //if we see console data then we get deletedCount
+                        // console.log(res)
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+
+            }
+        });
+    }
     return (
         <div>
             <SectionTitle
@@ -25,7 +74,7 @@ const AllUsers = () => {
             <div>
                 <h2>Total Users: {users.length}</h2>
                 <div className="overflow-x-auto">
-                    <table className="table table-zebra">
+                    <table className="table table-zebra w-full">
                         {/* head */}
                         <thead>
                             <tr>
@@ -44,11 +93,17 @@ const AllUsers = () => {
                                         <td>{user.name}</td>
                                         <td>{user.name}</td>
                                         <td>
+                                            { user.role === 'admin'? 
+                                               <><p>Admin</p></> :
+                                                <button onClick={() => handleMakeAdmin(user)} className="btn text-white btn-lg bg-orange-400">
 
-                                            <FaUsers></FaUsers>
+                                                    <FaUsers></FaUsers>
+                                                </button>
+                                            }
+
                                         </td>
                                         <td>
-                                            <button onClick={() => handleDelete(user._id)} className="btn text-red-600 btn-ghost btn-lg">
+                                            <button onClick={() => handleDeleteUser(user)} className="btn text-red-600 btn-ghost btn-lg">
 
                                                 <FaTrash></FaTrash>
                                             </button>
