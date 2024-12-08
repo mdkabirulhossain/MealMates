aac0ab3e27c7598caa21472d60150360aec7707f
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-08 12:55:27 +0600

    In auth provider add token for users

--- MealMates_Client/src/Providers/AuthProvider.jsx
+++ MealMates_Client/src/Providers/AuthProvider.jsx
@@ -2,12 +2,14 @@ import React, { createContext, useEffect, useState } from 'react';
 export const AuthContext = createContext(null);
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from '../firebase/firebase.config';
+import useAxiosPublic from '../hooks/useAxiosPublic';
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
+    const axiosPublic = useAxiosPublic();
 
     const createUser = (email, password)=>{
         setLoading(true)
@@ -38,10 +40,18 @@ const AuthProvider = ({children}) => {
             console.log('current user', currentUser);
             if(currentUser){
                 //get token and store client
+                const userInfo = {email: currentUser.email}
+                axiosPublic.post('/jwt', userInfo)
+                .then(res =>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token);
+                    }
+                })
             }
             else{
                 //TODO: remove token (if token stored in the client side: 
                 // Local storage, caching, in memory)
+                localStorage.removeItem('access-token');
             }
             setLoading(false);
         })
