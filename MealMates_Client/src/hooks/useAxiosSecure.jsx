20e7a6cc50f845bc1a5b1b73b2ff5830b7140f8f
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-08 18:02:42 +0600

    In useAxiosSecure use Interceptor response also

--- MealMates_Client/src/hooks/useAxiosSecure.jsx
+++ MealMates_Client/src/hooks/useAxiosSecure.jsx
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
 const axiosSecure = axios.create({
-    baseURL:'http://localhost:5000'
+  baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+
+  //request interceptor to add authorization for every secure call to the api
   axiosSecure.interceptors.request.use(function (config) {
-    
+
     const token = localStorage.getItem('access-token')
     console.log("Request stop by interceptors", token);
-   config.headers.authorization = `Bearer ${token}`
-   
+    config.headers.authorization = `Bearer ${token}`
+
     // Do something before request is sent
     return config;
   }, function (error) {
@@ -17,7 +19,18 @@ const useAxiosSecure = () => {
     return Promise.reject(error);
   })
 
-  return axiosSecure; 
+  //Intercepts 401 and 403 response
+  axiosSecure.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  })
+  
+  return axiosSecure;
 };
 
 export default useAxiosSecure;
