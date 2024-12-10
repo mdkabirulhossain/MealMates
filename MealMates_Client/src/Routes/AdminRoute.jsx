6fb227578b46600982c5229e08c24f914a0588f2
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-10 19:27:34 +0600

    Created Admin Routes

---
+++ MealMates_Client/src/Routes/AdminRoute.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import useAuth from '../hooks/useAuth';
+import useAdmin from '../hooks/useAdmin';
+import { Navigate, useLocation } from 'react-router-dom';
+
+const AdminRoute = ({children}) => {
+
+    const [user, loading] = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+
+    if (loading || isAdminLoading) {
+        return <span className="loading loading-spinner text-error"></span>
+    }
+    if (user && isAdmin) {
+        return children;
+    }
+    return <Navigate to='/login' state={{ from: location }}></Navigate>
+};
+
+export default AdminRoute;
