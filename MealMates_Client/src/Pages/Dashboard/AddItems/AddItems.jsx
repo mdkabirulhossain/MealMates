e85a8fb507ca205dc76140846b692bed1b4cc3a5
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-13 03:20:09 +0600

    Add item component is created

---
+++ MealMates_Client/src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddItems = () => {
+    return (
+        <div>
+            <h2>Hi this is Add Items page</h2>
+        </div>
+    );
+};
+
+export default AddItems;
--- MealMates_Client/src/Routes/Routes.jsx
+++ MealMates_Client/src/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import Secret from "../Pages/Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../Pages/Dashboard/Cart/Cart";
 import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
+import AddItems from "../Pages/Dashboard/AddItems/AddItems";
 
   export const router = createBrowserRouter([
     {
@@ -63,6 +64,10 @@ import AllUsers from "../Pages/Dashboard/AllUsers/AllUsers";
         {
           path:'allusers',
           element:<AllUsers/>
+        },
+        {
+          path:'additems',
+          element:<AddItems/>
         },
       ]
     },
