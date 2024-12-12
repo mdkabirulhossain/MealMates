2bd0a30bbc93c4f703111a88c413fb91c58c2520
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-12 23:46:24 +0600

    Check authorizied users

--- MealMates_Client/src/Pages/Shared/Secret/Secret.jsx
+++ MealMates_Client/src/Pages/Shared/Secret/Secret.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Secret = () => {
     return (
         <div>
-            
+            <h1>Just for check Only authorization user able to access</h1>
         </div>
     );
 };
