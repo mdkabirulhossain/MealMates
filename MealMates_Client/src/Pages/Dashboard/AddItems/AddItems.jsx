82317f91a64bc1b6a33de8a02a459dd9400806c2
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2024-12-15 08:44:20 +0600

    Add items form is created

--- MealMates_Client/src/Pages/Dashboard/AddItems/AddItems.jsx
+++ MealMates_Client/src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { useForm } from 'react-hook-form';
+import { FaUtensils } from "react-icons/fa";
 
 const AddItems = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+        
+        
+    }
     return (
         <div>
             <div>
@@ -10,6 +18,55 @@ const AddItems = () => {
                 >
 
                 </SectionTitle>
+
+            </div>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <label className="form-control w-full my-6">
+                        <div className="label">
+                            <span className="label-text">Name*</span>
+
+                        </div>
+                        <input {...register("name")} type="text" placeholder="Food Name" className="input input-bordered w-full" required />
+
+                    </label>
+                    <div className='flex gap-4'>
+                        <div className='w-full'>
+                            <label htmlFor="">Category*</label>
+                            <select {...register("category")} className="select select-bordered w-full " required>
+                                
+                                <option value="salad">salad</option>
+                                <option value="pizza">pizza</option>
+                                <option value="soup">soup</option>
+                                <option value="dessert">dessert</option>
+                                <option value="drinks">drinks</option>
+                            </select>
+                        </div>
+                        <div className='w-full'>
+                            <label className="form-control w-full">
+                                <label htmlFor="">Price*</label>
+                                <input {...register("price")} type="number" placeholder="Price" className="input input-bordered w-full" required />
+
+                            </label>
+                        </div>
+
+                    </div>
+                    <div className='my-6'>
+                        <label htmlFor="">Recipe Details*</label>
+                        <textarea
+                            {...register("recipe")}
+                            className="textarea textarea-bordered w-full" placeholder="Recipes Details" required >
+
+                        </textarea>
+                    </div>
+                    <div>
+                    <input {...register("image")} type="file" className="file-input w-full max-w-xs" />
+                    </div>
+                    <div className="flex justify-center  mt-6">
+                        <button className=" flex items-center py-4 px-5 text-white font-bold" style={{ background: "linear-gradient(90deg, #835D23 0%, #B58130 100%)" }}>Add Item <FaUtensils></FaUtensils> </button>
+                    </div>
+                </form>
             </div>
         </div>
     );
